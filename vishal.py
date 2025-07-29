c946b1b96a5fe7a0e1d31e4460a3c9415dbcf0b5
Vishalsingh-oo7 <vishalsingh.datasc@gmail.com>
2025-07-29 17:44:29 +0530

    Create vishal.py

---
+++ vishal.py
@@ -0,0 +1,15 @@
+# Reversing a string using slicing
+text = "PowerBI"
+reversed_text = text[::-1]
+print("Reversed String:", reversed_text)
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(29))  # True
